d11ffa793b571f4cf2f8e52b4f9428fbb0f2605f
liweirong <2277839278@qq.com>
2019-03-13 00:27:28 +0800

    本地map

--- DataMining/1_MapReduce/wordCount/map.py
+++ DataMining/1_MapReduce/wordCount/map.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-# 正则去符号，匹配字母,去标点等
+# 正则去符号，匹配字母和数字,去标点等
 p = re.compile(r'\w+')
 for line in sys.stdin:
     ss = line.strip().split(' ')
@@ -13,3 +13,15 @@ for line in sys.stdin:
 
 # 调试:
 # head -2 The_Man_of_Property.txt| python map_t.py
+
+# 直接读文件看效果
+# file = open("The_Man_of_Property.txt", 'r', encoding='UTF-8')
+# for line in file:
+#     strings = line.strip().split(" ")
+#     for string in strings:
+#         # 正则去除无关的东西
+#         if len(p.findall(string)) < 1:
+#             continue
+#         s_low = p.findall(string)[0].lower()
+#         print(s_low, "\t", 1)
+# file.close()
